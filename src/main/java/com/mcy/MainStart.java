41fad7fb4c1b041707b86a4d9ebd3c0c672ee6cd
sssscccchhhh <144687138+sssscccchhhh@users.noreply.github.com>
2023-09-17 14:14:32 +0800

    Add files via upload

---
+++ src/main/java/com/mcy/MainStart.java
@@ -0,0 +1,19 @@
+package com.mcy;
+import com.mcy.util.FileUtil;
+import com.mcy.util.TokenizerUtil;
+
+import java.util.List;
+import java.util.Map;
+
+
+public class MainStart {
+    public static void main(String[] args) {
+        String file1 = args[0];
+        String file2 = args[1];
+        Map<String, List<Integer>> stringListMap = TokenizerUtil.CountWord(file1);
+        Map<String, List<Integer>> stringListMap2 = TokenizerUtil.CountWord(file2);
+        Double aDouble = TokenizerUtil.CountCos(stringListMap, stringListMap2);
+        System.out.println(aDouble);
+        FileUtil.writeFile(args[2],String.valueOf(aDouble));
+    }
+}
